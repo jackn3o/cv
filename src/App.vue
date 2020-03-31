64042bbc65809181e10e8299813292c25e1c1790
unknown <jackn3o@gmail.com>
2020-04-01 01:34:58 +0800

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-content>
+      <Main />
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Main
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+body {
+  .theme--light.v-application {
+    background-color: #f0f0f6;
+    color: #5f6f81;
+    background-image: url(./assets/triangles-top.svg),
+      url(./assets/triangles-bottom.svg);
+    background-position: left top, right bottom;
+    background-repeat: no-repeat;
+    background-size: inherit;
   }
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row justify="center" fill-height no-gutters>
+      <v-col col="12" xl="6" md="9" xs="12">
+        <v-card
+          style="overflow:hidden; border-radius:32px 0 32px 32px; max-height: 720px; height:100%; min-height: 600px;"
+        >
+          <v-row no-gutters fill-height>
+            <v-col cols="3">
+              <v-card height="100%" color="red" outlined></v-card>
+            </v-col>
+            <v-col cols="9">
+              <v-card tile>222</v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Main",
+
+  data: () => ({})
+};
+</script>
+
